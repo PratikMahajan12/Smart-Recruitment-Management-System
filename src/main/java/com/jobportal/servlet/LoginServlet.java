5372dc8d5476f10069036560e95faed4dcc0b40b
PratikMahajan12 <83715741+PratikMahajan12@users.noreply.github.com>
2026-06-09 21:22:00 +0530

    Update LoginServlet.java

--- src/main/java/com/jobportal/servlet/LoginServlet.java
+++ src/main/java/com/jobportal/servlet/LoginServlet.java
@@ -1,1 +1,53 @@
+package com.jobportal.servlet;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.jobportal.util.DBUtil;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        try {
+
+            Connection con = DBUtil.getConnection();
+
+            String sql =
+                "SELECT * FROM users WHERE email=? AND password=?";
+
+            PreparedStatement ps =
+                con.prepareStatement(sql);
+
+            ps.setString(1, email);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                response.getWriter().println("Login Successful");
+            } else {
+                response.getWriter().println("Invalid Credentials");
+            }
+
+            con.close();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
