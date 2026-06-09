0b495cdcd1f878f9c05f8083c4ecb0d6167d52d6
PratikMahajan12 <83715741+PratikMahajan12@users.noreply.github.com>
2026-06-09 21:23:08 +0530

    Update DBUtil.java

--- src/main/java/com/jobportal/util/DBUtil.java
+++ src/main/java/com/jobportal/util/DBUtil.java
@@ -1,1 +1,34 @@
+package com.jobportal.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/jobportal";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            con = DriverManager.getConnection(
+                    URL,
+                    USER,
+                    PASSWORD);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
